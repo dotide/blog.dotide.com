749a7b85f5d27ac022958d69fbf7f3255dcec9e3
Michael Ding <yandy.ding@gmail.com>
2014-04-25 17:29:14 +0800

    Reconstruct project

--- en/extensions/links_helpers.rb
+++
@@ -1,8 +0,0 @@
-module LinksHelpers
-  def date_links date
-    year_link = "<a href='/en/#{date.year}.html'>#{date.year}</a>"
-    month_link = "<a href='/en/#{date.year}/%02d.html'>#{date.mon}</a>" % date.mon
-    day_link = "<a href='/en/#{date.year}/%02d/%02d.html'>#{date.mday}</a>" % [date.mon, date.mday]
-    return "#{year_link}/#{month_link}/#{day_link}"
-  end
-end
--- en/source/en/2013-09-05-jobs-hacker.html.markdown
+++
@@ -1,9 +0,0 @@
----
-title: Jobs for Hackers
-date: 2013-09-05
-author: Michael Ding
-tags:
-- jobs
----
-
-<script src="https://gist.github.com/yandy/6447170.js"></script>
